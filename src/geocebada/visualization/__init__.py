56485e4a48b9a7e89c22a76e8bc94539adc51de3
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-14 11:33:31 -0600

    feat: add reusable visualization package

---
+++ src/geocebada/visualization/__init__.py
@@ -0,0 +1,15 @@
+"""Reusable visualizations for notebooks and the GeoCebada app."""
+
+from geocebada.visualization.exploration import (
+    correlation_scatter,
+    distribution_figure,
+    missingness_table,
+    residual_diagnostic_figure,
+)
+
+__all__ = [
+    "correlation_scatter",
+    "distribution_figure",
+    "missingness_table",
+    "residual_diagnostic_figure",
+]
