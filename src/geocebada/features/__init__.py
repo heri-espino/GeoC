15fcdb4be55702a74f1e705fa89cc2a7e9cf80b0
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-14 11:33:23 -0600

    feat: expose interactive feature engineering API

--- src/geocebada/features/__init__.py
+++ src/geocebada/features/__init__.py
@@ -1,1 +1,15 @@
 """Feature engineering utilities."""
+
+from geocebada.features.interactive import (
+    FeatureRecipe,
+    apply_feature_recipe,
+    feature_recipe_to_dict,
+    merge_features,
+)
+
+__all__ = [
+    "FeatureRecipe",
+    "apply_feature_recipe",
+    "feature_recipe_to_dict",
+    "merge_features",
+]
