81c65163455d373dcbf6a0a95b9f47b726fb66b7
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-14 14:17:11 -0600

    feat(viz): export multivariate and map visualizations

--- src/geocebada/visualization/__init__.py
+++ src/geocebada/visualization/__init__.py
@@ -1,15 +1,23 @@
 """Reusable visualizations for notebooks and the GeoCebada app."""
 
 from geocebada.visualization.exploration import (
+    correlation_heatmap,
     correlation_scatter,
     distribution_figure,
     missingness_table,
+    outlier_summary,
+    pairplot_figure,
     residual_diagnostic_figure,
 )
+from geocebada.visualization.geospatial import parcel_map_figure
 
 __all__ = [
+    "correlation_heatmap",
     "correlation_scatter",
     "distribution_figure",
     "missingness_table",
+    "outlier_summary",
+    "pairplot_figure",
+    "parcel_map_figure",
     "residual_diagnostic_figure",
 ]
