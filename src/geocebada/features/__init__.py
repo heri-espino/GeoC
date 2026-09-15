350c2e0474b545f02f169d47e194717da9729d49
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-14 21:01:43 -0600

    feat(features): export temporal alignment utilities

--- src/geocebada/features/__init__.py
+++ src/geocebada/features/__init__.py
@@ -6,10 +6,22 @@ from geocebada.features.interactive import (
     feature_recipe_to_dict,
     merge_features,
 )
+from geocebada.features.temporal import (
+    align_temporal_knn,
+    aligned_to_wide,
+    make_temporal_grid,
+    temporal_backend_available,
+    temporal_coverage_summary,
+)
 
 __all__ = [
     "FeatureRecipe",
+    "align_temporal_knn",
+    "aligned_to_wide",
     "apply_feature_recipe",
     "feature_recipe_to_dict",
+    "make_temporal_grid",
     "merge_features",
+    "temporal_backend_available",
+    "temporal_coverage_summary",
 ]
