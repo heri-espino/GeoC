f0b109db31f16c16a9fc6fba00864f56ebb76f15
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-18 06:09:18 -0600

    Export external data normalization helpers

--- src/geocebada/data/__init__.py
+++ src/geocebada/data/__init__.py
@@ -1,5 +1,10 @@
 """Data ingestion, validation and cleaning utilities."""
 
+from geocebada.data.external import (
+    build_admin_mapping,
+    load_inegi_municipalities,
+    load_siap_barley_history,
+)
 from geocebada.data.files import concatenate_csvs, discover_files
 from geocebada.data.filtering import filter_frame
 from geocebada.data.official import load_basic_data, load_official_tabular, load_pro_data
@@ -18,6 +23,9 @@ from geocebada.data.targets import (
 
 __all__ = [
     "attach_yield_split_metadata",
+    "load_siap_barley_history",
+    "load_inegi_municipalities",
+    "build_admin_mapping",
     "build_raster_inventory",
     "concatenate_csvs",
     "discover_files",
