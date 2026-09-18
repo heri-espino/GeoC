0dbb8ea8c0d6aec1c9680edb6bf00cf47bf5d2e9
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-18 06:09:21 -0600

    Export parcel feature engineering API

--- src/geocebada/features/__init__.py
+++ src/geocebada/features/__init__.py
@@ -6,6 +6,20 @@ from geocebada.features.interactive import (
     feature_recipe_to_dict,
     merge_features,
 )
+from geocebada.features.parcel import (
+    build_base_features,
+    build_cem15_features,
+    build_daily_chirps_features,
+    build_official_climate_features,
+    build_satellite_features,
+    build_siap_features,
+    build_soilgrids_features,
+    build_static_raster_features,
+    build_wapor_features,
+    merge_feature_blocks,
+    validate_parcel_feature_table,
+    zonal_stats_for_raster,
+)
 from geocebada.features.temporal import (
     align_temporal_knn,
     aligned_to_wide,
@@ -16,6 +30,18 @@ from geocebada.features.temporal import (
 
 __all__ = [
     "FeatureRecipe",
+    "zonal_stats_for_raster",
+    "validate_parcel_feature_table",
+    "merge_feature_blocks",
+    "build_wapor_features",
+    "build_static_raster_features",
+    "build_soilgrids_features",
+    "build_siap_features",
+    "build_satellite_features",
+    "build_official_climate_features",
+    "build_daily_chirps_features",
+    "build_cem15_features",
+    "build_base_features",
     "align_temporal_knn",
     "aligned_to_wide",
     "apply_feature_recipe",
