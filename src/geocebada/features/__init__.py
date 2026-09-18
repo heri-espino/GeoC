566ad54f80c6efd4830de2e659aaeaa7fa6a36ab
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-18 12:00:45 -0600

    Export agronomic feature utilities

--- src/geocebada/features/__init__.py
+++ src/geocebada/features/__init__.py
@@ -1,6 +1,6 @@
 """Feature engineering utilities."""
 
-from geocebada.features.interactive import (
+from geocebada.features.agronomic import (\n    build_agronomic_feature_layer,\n    join_agronomic_features,\n    validate_agronomic_feature_layer,\n)\nfrom geocebada.features.interactive import (
     FeatureRecipe,
     apply_feature_recipe,
     feature_recipe_to_dict,
