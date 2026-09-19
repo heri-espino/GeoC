f26f78d756a001b843eb098593ffc094547ba456
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-18 18:46:48 -0600

    Export Checkpoint 03C evaluation API

--- src/geocebada/evaluation/__init__.py
+++ src/geocebada/evaluation/__init__.py
@@ -1,5 +1,13 @@
 """Model evaluation, validation and diagnostics utilities."""
 
+from geocebada.evaluation.checkpoint03c import (
+    RepresentationSpec,
+    build_layer_feature_catalog,
+    build_representation_specs,
+    evaluate_representation_benchmark,
+    make_representation_estimator,
+    summarize_representation_benchmark,
+)
 from geocebada.evaluation.parcel_modeling import (
     build_feature_catalog,
     build_fixed_fold_assignments,
@@ -19,6 +27,12 @@ from geocebada.evaluation.regression import (
 )
 
 __all__ = [
+    "RepresentationSpec",
+    "build_layer_feature_catalog",
+    "build_representation_specs",
+    "evaluate_representation_benchmark",
+    "make_representation_estimator",
+    "summarize_representation_benchmark",
     "benchmark_regressors",
     "build_feature_catalog",
     "build_fixed_fold_assignments",
