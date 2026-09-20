c655da5583750712c9404a433409b4ec9fcf5774
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-20 07:53:55 -0600

    Export Checkpoint 04A figures

--- src/geocebada/visualization/__init__.py
+++ src/geocebada/visualization/__init__.py
@@ -1,5 +1,17 @@
 """Reusable visualizations for notebooks and the GeoCebada app."""
 
+from geocebada.visualization.checkpoint04a import (
+    plot_adversarial_roc,
+    plot_moran_scatter,
+    plot_nearest_distance_distribution,
+    plot_pca_embedding,
+    plot_primary_temporal_neighbor_panels,
+    plot_similarity_vs_yield_difference,
+    plot_support_ranking,
+    plot_temporal_similarity_distribution,
+    plot_top_feature_shift,
+    plot_train_target_map,
+)
 from geocebada.visualization.exploration import (
     correlation_heatmap,
     correlation_scatter,
@@ -12,6 +24,16 @@ from geocebada.visualization.exploration import (
 from geocebada.visualization.geospatial import parcel_map_figure
 
 __all__ = [
+    "plot_adversarial_roc",
+    "plot_moran_scatter",
+    "plot_nearest_distance_distribution",
+    "plot_pca_embedding",
+    "plot_primary_temporal_neighbor_panels",
+    "plot_similarity_vs_yield_difference",
+    "plot_support_ranking",
+    "plot_temporal_similarity_distribution",
+    "plot_top_feature_shift",
+    "plot_train_target_map",
     "correlation_heatmap",
     "correlation_scatter",
     "distribution_figure",
