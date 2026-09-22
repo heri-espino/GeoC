a5000cec118ce9a227626670c3f563540188d61e
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-21 19:03:13 -0600

    Export Checkpoint 05 model bundle helpers

--- src/geocebada/models/__init__.py
+++ src/geocebada/models/__init__.py
@@ -1,1 +1,15 @@
 """Model training, persistence and inference utilities."""
+
+from geocebada.models.checkpoint05 import (
+    fixed_target_diagnostics,
+    fixed_target_predictions,
+    load_checkpoint05_bundle,
+    verify_fixed_target_bundle,
+)
+
+__all__ = [
+    "fixed_target_diagnostics",
+    "fixed_target_predictions",
+    "load_checkpoint05_bundle",
+    "verify_fixed_target_bundle",
+]
