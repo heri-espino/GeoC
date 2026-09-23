551274de81513cb0b268e55e98b8fc095162b46e
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-23 04:36:53 -0600

    Fix ONNX model helper exports

--- src/geocebada/models/__init__.py
+++ src/geocebada/models/__init__.py
@@ -6,6 +6,10 @@ from geocebada.models.checkpoint05 import (
     load_checkpoint05_bundle,
     verify_fixed_target_bundle,
 )
+from geocebada.models.onnx_export import (
+    export_regressor_to_onnx,
+    verify_onnx_regressor,
+)
 
 __all__ = [
     "export_regressor_to_onnx",
